e0ef5591b919a074158ba27a53eeffff97eb5c0e
pakosam <pako.s@yahoo.com>
2022-04-06 21:21:57 +0200

    Evo ga!

--- MojProjekt.js
+++ MojProjekt.js
@@ -1,3 +1,27 @@
 function redirect (link){
     window.location.href=link
 }
+
+$('#view-informacije').on('click', function(){
+    const images = $('#informacijeOProizvodima').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1600);
+});
+
+$('#view-ponuda').on('click', function(){
+    const images = $('#ponuda').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1600);
+});
+
+$('#view-iskustva').on('click', function(){
+    const images = $('#iskustva').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 1600);
+});
